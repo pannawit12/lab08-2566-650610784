47d9646d86aba6e8dcbeb4449b9a5b5b01e8325c
Your Name <you@example.com>
2023-07-19 17:32:58 +0700

    fish

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-export const Header = (props) => {
+export const Header = () => {
   return (
     <>
       <h4 className="display-4 fst-italic text-center">Todo List Mock Up</h4>
--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,29 @@
+import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { Task } from "../../components/Task";
+import { TaskInput } from "../../components/TaskInput";
 
 export default function Home() {
   return (
     // Main container
     <div className="container mx-auto">
       {/* header section */}
-      <Header></Header>
+      <Header />
 
       {/* tasks container */}
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* task input */}
-        <div className="d-flex gap-1">
-          <input
-            className="form-control"
-            placeholder="Insert a task here.."
-          ></input>
-          <button className="btn btn-primary">Add</button>
-        </div>
+        <TaskInput />
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Read a book</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Read a book"></Task>
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Take a shower</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Take a shower"></Task>
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Sleep</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task title="Sleep"></Task>
       </div>
 
       {/* //footer section */}
-      <div className="mt-3">
-        <p className="text-secondary text-center">
-          copyright © 2023 Pannawit Suatap 12345678
-        </p>
-      </div>
+      <Footer year="2023" fullName="Pannawit Setsiriwanit" studentId="650610784"></Footer>
     </div>
   );
 }
