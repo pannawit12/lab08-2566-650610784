5ae3223f9c52a38720b4bbcae7d7eb1567db54fc
Your Name <you@example.com>
2023-07-19 17:35:00 +0700

    tasktitle edit

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,7 +1,7 @@
 export const Task = (props) => {
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      <span>props.title</span>
+      <span>{props.title}</span>
       <button className="btn btn-success">Done</button>
       <button className="btn btn-danger">Delete</button>
     </div>
